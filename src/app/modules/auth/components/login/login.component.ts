bdc67a1495472a39a0917931f52b02a456cb2e68
Nixon <33127032+nx96@users.noreply.github.com>
2021-04-13 10:17:01 -0500

    Login screen

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -10,6 +10,8 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  startImageUrl = '../assets/img/startlogin.png';
+
   public user:string;
   public password:string;
   public loader: boolean

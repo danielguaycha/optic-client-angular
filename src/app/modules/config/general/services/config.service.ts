17af9b74ff3ec6119b47875d5587cb4d126b13a3
Nixon <33127032+nx96@users.noreply.github.com>
2021-04-17 01:44:58 -0500

    Formulario Configuraciones

--- src/app/modules/config/general/services/config.service.ts
+++ src/app/modules/config/general/services/config.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 import {SecureStorageService} from '../../../auth/services/secure-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
+  [x: string]: any;
 
   public iva: number
   public decimals: number;
   public config: any;
-  constructor(private storage: SecureStorageService) {
+  constructor(private storage: SecureStorageService, private http: HttpClient) {
     const rawCfg = this.storage.get('config');
     if (rawCfg) {
       this.config = JSON.parse(rawCfg);
@@ -34,5 +37,9 @@ export class ConfigService {
 
   addPercent(neto: number, tarifaPercent) {
     return this.toFloat(neto * (1 + (tarifaPercent/100)));
+  }
+
+  saveConfg(data: any) : Observable<any> {
+    return this.http.put('enterprise/config', data);
   }
 }
